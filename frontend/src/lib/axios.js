69b4e5db7245ba03a6a98e3b1bd14432b3e949e8
zayn-tech-info <zayntech.info@gmail.com>
2025-09-07 15:08:27 +0100

    fix: update axios baseURL to production API endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1/",
+  baseURL: "https://products-api-7byt.onrender.com/",
   withCredentials: true,
 });
