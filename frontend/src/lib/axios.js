bff4d18238cd74c454f994b3521fc6ad45b4c21a
zayn-tech-info <zayntech.info@gmail.com>
2025-09-09 00:01:12 +0100

    fix: update API endpoint paths in axios instance and useAuthStore for authentication

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://temu-clone-backed.onrender.com/api/v1/auth/",
+  baseURL: "https://temu-clone-backed.onrender.com/",
   withCredentials: true,
 });
