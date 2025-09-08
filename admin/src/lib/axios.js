62fc38835752dd2f2a098ef0bae1fde96041a8d9
zayn-tech-info <zayntech.info@gmail.com>
2025-09-08 23:46:18 +0100

    fix: comment out Authorization header in axios instance

--- admin/src/lib/axios.js
+++ admin/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://temu-clone-backed.onrender.com/api/v1/",
   withCredentials: true,
-  headers: {
+/*   headers: {
     Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YmRkN2Y2MjdhMjU1NWE0M2Y1YzE4ZCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzU3MzY5ODUzLCJleHAiOjE3NTc5NzQ2NTN9.Aim_-4yYq5TnS_QXFIVxPzE-oIpN1IFEj42ypgygdNk`,
-  },
+  }, */
 });
