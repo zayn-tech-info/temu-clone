96640617a414034c7eac1390e7b7e3bc71a877e4
zayn-tech-info <zayntech.info@gmail.com>
2025-09-09 00:09:43 +0100

    fix: update Authorization header value in axios instance

--- admin/src/lib/axios.js
+++ admin/src/lib/axios.js
@@ -3,6 +3,6 @@ export const axiosInstance = axios.create({
   baseURL: "https://temu-clone-backed.onrender.com/api/v1/",
   withCredentials: true,
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YmRkN2Y2MjdhMjU1NWE0M2Y1YzE4ZCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzU3MzcyODMyLCJleHAiOjE3NTc5Nzc2MzJ9.JzXkZ492YGhiD07Tk5hmGKegOMnaMsjrZWSZkyeqEsA`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YmRkN2Y2MjdhMjU1NWE0M2Y1YzE4ZCIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc1NzM3Mjk0NiwiZXhwIjoxNzU3OTc3NzQ2fQ.y_XcCUpZeLTau8Gk8tn6NV_BEWjybIqHghLkNdHROss`,
   },
 });
