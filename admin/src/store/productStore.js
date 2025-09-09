0745596c7f69f6a81a996e00fdb49a11a24f440a
zayn-tech-info <zayntech.info@gmail.com>
2025-09-09 10:34:07 +0100

    fix: enhance logging for product addition and image handling in addProduct function

--- admin/src/store/productStore.js
+++ admin/src/store/productStore.js
@@ -8,6 +8,9 @@ export const useProductStore = create((set, get) => ({
   addProduct: async (productData) => {
     set({ isAddingProduct: true });
     try {
+      console.log("Original productData:", productData);
+      console.log("Images array:", productData.images);
+
       const formData = new FormData();
 
       Object.keys(productData).forEach((key) => {
@@ -17,9 +20,19 @@ export const useProductStore = create((set, get) => ({
       });
 
       if (productData.images && productData.images.length > 0) {
-        productData.images.forEach((file) => {
+        console.log("Adding images to FormData:", productData.images);
+        productData.images.forEach((file, index) => {
+          console.log(`File ${index}:`, file);
           formData.append("images", file);
         });
+      } else {
+        console.log("No images found in productData");
+      }
+
+      // Log FormData contents
+      console.log("FormData contents:");
+      for (let [key, value] of formData.entries()) {
+        console.log(key, value);
       }
 
       const res = await axiosInstance.post("products", formData, {
@@ -29,10 +42,6 @@ export const useProductStore = create((set, get) => ({
       });
 
       console.log("Response:", res.data);
-      console.log("Product Data:", productData);
-
-      /*       const currentProducts = get().products;
-      set({ products: [...currentProducts, res.data] }); */
 
       toast.success("New product added");
     } catch (error) {
